b98c13d5842345a855a669827717db78ab7fe770
KoheiOkamoto <koko72.flavor@gmail.com>
2020-01-19 01:01:09 +0900

    蟻本1-6の結果とAtCoderのpracticeコンテストの結果をコミット

---
+++ ant_book/1-6/triangle.py
@@ -0,0 +1,23 @@
+# 整数の入力
+N = int(input())
+# スペース区切りの整数の入力
+A_list = [int(e) for e in input().split()]
+
+ANS = 0
+
+for I in range(0,N):
+    for J in range(I+1,N):
+        for K in range(J+1,N):
+            LEN = A_list[I]+A_list[J]+A_list[K]
+            MAX = max(A_list[I],A_list[J],A_list[K])
+            REST = LEN - MAX
+
+            if REST > MAX:
+              ANS = max(ANS,LEN)
+
+print(ANS)
+
+
+
+
+
